0f753726a54b39178cde139d09eba9f272478f9b
admin <admin@itcast.cn>
2020-07-08 17:00:22 +0800

    gmall-user提交 7/8

---
+++ gmall-user/src/main/java/com/icj/gmall/user/mapper/UmsMemberReceiveAddressMapper.java
@@ -0,0 +1,8 @@
+package com.icj.gmall.user.mapper;
+
+import com.icj.gmall.user.bean.UmsMemberReceiveAddress;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface UmsMemberReceiveAddressMapper extends Mapper<UmsMemberReceiveAddress> {
+
+}
